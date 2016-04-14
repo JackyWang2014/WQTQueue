09aa84de52d8ae3288a2d0d1f52e5102593132a5
JackyWang2014 <339847016@qq.com>
2016-04-14 15:56:59 +0800

    Initial

---
+++ WQTQueue/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  WQTQueue
+//
+//  Created by 王亓泰 on 16/4/14.
+//  Copyright © 2016年 WangQitai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
